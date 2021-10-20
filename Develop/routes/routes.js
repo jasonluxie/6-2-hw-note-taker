6ba430e6541484697499003988cddfd7038ec2ee
Juri <jasonluxie@gmail.com>
2021-10-19 20:47:07 -0500

    worked on post function

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -1,11 +1,36 @@
-const path = require('path');
-const app = express()
+const path = require("path");
+const express = require("express");
+const app = express();
 
+const notesGet = () => {
+    app.get("/notes", (req, res) => {
+        res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+    });
 
-app.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/notes.html'))
-})
+    app.post("notes", (req, res) => {
+        const { title, text } = req.body;
+        if (title && text) {
+            const newNote = {
+                title,
+                text,
+                id: uniqid(),
+            };
+            const stringNote = JSON.stringify(newNote);
+            fs.readFile("./db/db.json", "utf8", (err, data) => {
+                if (err) throw err;
+                const noteData = JSON.parse(data);
+                // console.log(bookData);
+                noteData.push(stringNote);
+                fs.writeFile(
+                    "./db/db.json",
+                    JSON.stringify(noteData, null, 4),
+                    (err) => {
+                        console.log("added to the file");
+                    }
+                );
+            });
+        } else res.status(500).json("Error in saving note");
+    });
+};
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, './Develop/public/index.html'))
-);
+module.exports = notesGet();
--- server.js
+++ server.js
@@ -1,40 +1,49 @@
-const express = require ('express')
-const path = require('path');
-const route = require('./Develop/routes/routes')
-const uniqid = require('uniqid')
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+// const route = require("./Develop/routes/routes");
+const uniqid = require("uniqid");
 // console.log(newid)
-const newid = uniqid()
-const app = express()
+const newid = uniqid();
+const app = express();
 const PORT = 3001;
 
 //middleware for parsing objects
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //middleware for sending static files like css
-app.use(express.static('public'));
+app.use(express.static("Develop/public"));
 
-app.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/notes.html'))
-})
-app.post('notes', (req,res) => {
-    const { title, text } = req.body
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+});
+
+app.post("notes", (req, res) => {
+    const { title, text } = req.body;
     if (title && text) {
         const newNote = {
             title,
             text,
-            id: uniqid()
-        }
-    }
-})
+            id: uniqid(),
+        };
+        const stringNote = JSON.stringify(newNote);
+        fs.readFile("./db/db.json", "utf8", (err, data) => {
+            if (err) throw err;
+            const noteData = JSON.parse(data);
+            // console.log(bookData);
+            noteData.push(stringNote);
+            fs.writeFile(
+                "./db/db.json",
+                JSON.stringify(noteData, null, 4),
+            );
+        });
+    } else res.status(500).json("Error in saving note");
+});
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, './Develop/public/index.html'))
+app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"))
 );
 
-
-
-
-
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+    console.log(`App listening at http://localhost:${PORT}`)
 );
