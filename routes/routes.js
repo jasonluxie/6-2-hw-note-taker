ea9c017506e9e74cd238d454126d8fd634c4c353
Juri <jasonluxie@gmail.com>
2021-10-20 21:12:54 -0500

    added test app.delete

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const fs = require('fs')
+const fs = require("fs");
 const path = require("path");
 const app = express();
 const uniqid = require("uniqid");
-const notesDB = require('../db/db.json')
+const notesDB = require("../db/db.json");
 
 app.get("/notes", (req, res) => {
     res.json(notesDB);
@@ -18,14 +18,38 @@ app.post("/notes", (req, res) => {
             id: uniqid(),
         };
         fs.readFile("./db/db.json", "utf8", (err, data) => {
-            if (err) {console.error(err)};
+            if (err) {
+                console.error(err);
+            }
             const noteData = JSON.parse(data);
             noteData.push(newNote);
-            fs.writeFile("./db/db.json", JSON.stringify(noteData, null, 4), (err, result) => {
-                if (err) console.error(err)
-            });
+            fs.writeFile(
+                "./db/db.json",
+                JSON.stringify(noteData, null, 4),
+                (err, result) => {
+                    if (err) console.error(err);
+                }
+            );
         });
     } else res.status(500).json("Error in saving note");
 });
+
+// Need to add req/res somewhere
+// app.delete(`notes/${id}`, (req, res) => {
+//     fs.readFile("./db/db.json", "utf8", (err, data) => {
+//         if (err) {
+//             console.error(err);
+//         }
+//         const noteData = JSON.parse(data);
+//         const newNoteData = noteData.filter((e) => e.id != `${id}`);
+//         fs.writeFile(
+//             "./db/db.json",
+//             JSON.stringify(newNoteData, null, 4),
+//             (err, result) => {
+//                 if (err) console.error(err);
+//             }
+//         );
+//     });
+// });
 
 module.exports = app;
