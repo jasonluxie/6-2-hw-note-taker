5e8e18e83329cc87cede3e50270a2b165694bc3b
Juri <jasonluxie@gmail.com>
2021-10-21 10:06:17 -0500

    trying to connect to heroku

--- server.js
+++ server.js
@@ -20,11 +20,6 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-// Do I need this?
-// app.post("/notes", (req, res) => {
-//     res.sendFile(path.join(__dirname, "./public/notes.html"));
-// });
-
 app.get("*", (req, res) =>
     res.sendFile(path.join(__dirname, "./public/index.html"))
 );
